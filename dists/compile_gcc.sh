8dc207fe7c7448321b3bcccd10030f1f8673dcb3
Borja Lorente <blorente@ucm.es>
2016-11-07 21:22:37 +0100

    Add compile files

---
+++ dists/compile_gcc.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+if [[ $( pwd ) == *"/dists" ]]
+then
+  cd ..
+fi
+ROOTDIR=$( pwd )
+
+if [[ -d "bin" ]]
+then
+  rm -rf bin
+fi
+
+mkdir bin
+COMPILEDIR="$ROOTDIR/bin"
+
+cd $COMPILEDIR
+cmake -G "Unix Makefiles" ..
+cmake --build .
+
+echo -n "Run tests? [y/n]: "
+read repl
+if [ $repl == "y" ] ; then
+  cd tests
+  ./tests
+fi
