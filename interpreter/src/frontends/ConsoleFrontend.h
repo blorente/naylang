05caf0e975b568eb97429f331af7e2a3563e73eb
Borja Lorente <blorente@ucm.es>
2017-04-10 12:48:48 +0200

    REmove antlr for console frontend

--- interpreter/src/frontends/ConsoleFrontend.h
+++ interpreter/src/frontends/ConsoleFrontend.h
@@ -7,17 +7,15 @@
 #define NAYLANG_CONSOLEFRONTENDLISTENER_H
 
 #include <core/control/Interpreter.h>
-#include <ConsoleFrontendCommandsBaseListener.h>
 
 namespace naylang {
-class ConsoleFrontend : public ConsoleFrontendCommandsBaseListener {
+class ConsoleFrontend {
 
     Interpreter interpreter;
 
 public:
     void runCommand(std::string command);
 
-    void exitExecCommand(ConsoleFrontendCommandsParser::ExecCommandContext *ctx) override;
 };
 }
 #endif //NAYLANG_CONSOLEFRONTENDLISTENER_H
